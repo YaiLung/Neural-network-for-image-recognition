3ebfb68f75b25edacb1511ad58570517f8d56da3
Никита <bonbarcovoleg55@gmail.com>
2025-02-20 20:33:02 +0300

    new test

---
+++ tests/test_prediction.py
@@ -0,0 +1,14 @@
+import numpy as np
+from main import model, SIZE
+
+def test_prediction():
+    # Создаём случайный "грустный" смайлик
+    test_img = np.zeros((SIZE, SIZE, 3))
+    test_img = test_img.reshape(1, SIZE, SIZE, 3) / 255.0
+
+    # Предсказываем класс
+    pred = model.predict(test_img)
+    pred_class = np.argmax(pred)
+
+    # Проверяем, что предсказание в пределах [0,1]
+    assert pred_class in [0, 1], "Неверный класс!"
