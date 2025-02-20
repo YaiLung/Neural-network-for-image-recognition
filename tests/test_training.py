ae7e3427e96c187f3cef2e11faf8bc956df44144
Никита <bonbarcovoleg55@gmail.com>
2025-02-20 22:20:05 +0300

    Update test_training.py

--- tests/test_training.py
+++ tests/test_training.py
@@ -1,6 +1,11 @@
+import sys
+from unittest.mock import MagicMock
+
+# Подмена Tkinter фиктивным объектом перед импортом main.py
+sys.modules["tkinter"] = MagicMock()
+
 import numpy as np
 from main import model, train_images, train_labels
-
 
 def test_training():
     # Проверяем, что модель корректно обучается
